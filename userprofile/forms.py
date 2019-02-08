45405a274125947b1efcec4e526bb25352b904fd
wei.guo <17647361832@163.com>
2019-02-08 15:51:12 +0800

    after regital

---
+++ userprofile/forms.py
@@ -0,0 +1,38 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author: P♂boy
+@License: (C) Copyright 2013-2017, Node Supply Chain Manager Corporation Limited.
+@Contact: 17647361832@163.com
+@Software: Pycharm
+@File: forms.py
+@Time: 2019/2/7 13:45
+@Desc:
+"""
+
+from django import forms
+
+from django.contrib.auth.models import User
+
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField()
+
+# 用户注册表单类
+class UserRegisterForm(forms.ModelForm):
+    password = forms.CharField()
+    password2 = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+    def clean_password2(self):
+        data = self.cleaned_data
+        if data.get('password') == data.get('password2'):
+            return data.get('password')
+
+        else:
+            raise forms.ValidationError("密码输入不一致，请重试")
